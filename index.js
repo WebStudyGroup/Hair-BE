34f1dc70fe6d048fa278065e3f12786ed4fc884d
duri <bluekmky@likelion.org>
2021-03-23 22:02:18 +0900

    db Connect

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ import helmet from 'helmet';
 import cookierParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose'
 import routers from './routers';
+
+
+mongoose.connect('mongodb+srv://bluekmky:mIQAnlkhW5sErg7W@hairhair.fwzva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: true
+}).then(()=> console.log('mongoDb connect'))
+  .catch(err => console.log(err))
 
 dotenv.config();
 const app = express();
